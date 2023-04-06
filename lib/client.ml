732821f1246b200acf18b864221965167078d34a
Shubham <kumar.shubham1312@gmail.com>
2023-04-06 10:26:04 +0530

    [wip] : refactored everything except RTT

--- lib/client.ml
+++ lib/client.ml
@@ -1,4 +1,5 @@
 open Lwt.Infix
+open Lwt.Syntax
 module Util = Chatlib.Util
 module Bytes = Chatlib.Util.Bytes
 
@@ -9,71 +10,17 @@ module Bytes = Chatlib.Util.Bytes
     and when they are resolved they get a chance to generate result
 *)
 
-let client_trip = ref @@ Util.init_trip ()
-let client_socket = Lwt_unix.(socket PF_INET SOCK_STREAM 0)
+(* let client_trip = ref @@ Util.init_trip () *)
 
-let client_addr port =
-  match
-    Lwt_unix.(connect client_socket @@ ADDR_INET (Unix.inet_addr_any, port))
-  with
-  | exception _ -> Lwt_io.printf "Cannot connect to server at port : %d" port
-  | _ -> Lwt.return_unit
-
-let rec handle_send () =
-  Lwt_io.(write_line stdout @@ Printf.sprintf "Enter message : ") >>= fun _ ->
-  Lwt_io.read_line_opt Lwt_io.stdin >>= fun input ->
-  match input with
-  | Some msg ->
-      let bytes_msg = Bytes.of_string msg in
-      Lwt_io.(write_line stdout @@ Printf.sprintf "Sending message : %s" msg)
-      >>= fun _ ->
-      (* send data as bytes to the server *)
-      Lwt_unix.write client_socket bytes_msg 0 (Bytes.length bytes_msg)
-      >>= fun _ ->
-      (* initialize start time *)
-      client_trip :=
-        Util.update_trip !client_trip (Unix.gettimeofday ())
-          !client_trip.end_time;
-      Lwt.choose [ handle_send (); handle_recv () ]
-  | None -> Lwt.choose [ handle_send (); handle_recv () ]
-
-and handle_recv () =
-  let buffer = Bytes.create 1024 in
-  (* receive data as bytes from the server *)
-  Lwt_unix.read client_socket buffer 0 1024 >>= fun len ->
-  (* initialize end time *)
-  client_trip :=
-    Util.update_trip !client_trip !client_trip.start_time
-    @@ Unix.gettimeofday ();
-  let buffer = Bytes.to_string @@ Bytes.filter_unfilled_bytes buffer '\000' in
-  (* check if buffer is empty *)
-  if len == 0 then handle_recv ()
-  else
-    (* check if it is an acknowledgement or data sent from the server *)
-    match buffer with
-    | "message received" ->
-        Lwt_io.(
-          write_line stdout
-          @@ Printf.sprintf "Received message: %s\nRoundtrip : %f secs" buffer
-          @@ (!client_trip.end_time -. !client_trip.start_time))
-        >>= fun _ ->
-        client_trip := Util.init_trip ();
-        Lwt.choose [ handle_send (); handle_recv () ]
-    | _ ->
-        Lwt_unix.write client_socket Util.ack 0 Util.ack_len >>= fun _ ->
-        Lwt_io.(
-          write_line stdout @@ Printf.sprintf "Received message: %s" buffer)
-        >>= fun _ ->
-        (* re-initialize the value for the next trip *)
-        client_trip := Util.init_trip ();
-        Lwt.choose [ handle_send (); handle_recv () ]
-
-let start_client port =
-  client_addr port >>= fun _ ->
+let start_client host port =
+  let* client_socket, _ = Util.create_client_socket host port in
   let rec loop () =
-    (* wait for promise resolution *)
-    Lwt.choose [ handle_recv (); handle_send () ] >>= fun _ -> loop ()
+    Lwt.choose
+      [
+        Util.send_handler client_socket; Util.client_recv_handler client_socket;
+      ]
+    >>= loop
   in
   loop ()
 
-let main port = Lwt_main.run @@ start_client port
+let main host port = Lwt_main.run @@ start_client host port
--- lib/util.ml
+++ lib/util.ml
@@ -1,3 +1,6 @@
+open Lwt.Infix
+open Lwt.Syntax
+
 module Bytes = struct
   include Bytes
 
@@ -20,10 +23,106 @@ let update_trip trip start_time end_time =
   trip.end_time <- end_time;
   trip
 
-let ack = Bytes.of_string "message received"
-let ack_len = Bytes.length ack
+let ack = "message received"
+let ack_bytes = Bytes.of_string ack
+let ack_len = Bytes.length ack_bytes
 
 let sockaddr_to_string (sockaddr : Unix.sockaddr) : string =
   match sockaddr with
-  | ADDR_INET (i, _) -> Unix.string_of_inet_addr i
+  | ADDR_INET (addr, port) ->
+      Printf.sprintf "%s:%d" (Unix.string_of_inet_addr addr) port
   | ADDR_UNIX s -> s
+
+let create_socket () = Lwt_unix.(socket PF_INET SOCK_STREAM 0)
+
+let close_client socket =
+  Lwt_unix.close socket >>= fun _ -> Lwt_io.printf "Client disconnected"
+
+let get_sockaddr host port = Unix.(ADDR_INET (inet_addr_of_string host, port))
+
+let create_client_socket host port =
+  let socket = create_socket () in
+  let sockaddr = get_sockaddr host port in
+  (match Lwt_unix.connect socket sockaddr with
+  | exception _ ->
+      Lwt_io.printf "Cannot connect to server at : [%s:%s]" host
+        (sockaddr_to_string sockaddr)
+  | _ -> Lwt.return_unit)
+  >>= fun _ -> Lwt.return (socket, sockaddr)
+
+let create_server_socket host port =
+  let socket = create_socket () in
+  let sockaddr = get_sockaddr host port in
+  (match Lwt_unix.bind socket sockaddr with
+  | exception e ->
+      Lwt_io.printlf "Cannot bind to client at : [%s:%s]" host
+        (sockaddr_to_string sockaddr)
+      >>= fun _ -> Lwt.fail e
+  | _ -> Lwt.return_unit)
+  >>= fun _ -> Lwt.return (socket, sockaddr)
+
+let rec send_handler socket =
+  Lwt_io.printl "Enter message : " >>= fun _ ->
+  Lwt_io.(read_line_opt stdin) >>= fun input ->
+  match input with
+  | Some msg ->
+      let bytes_msg = Bytes.of_string msg in
+      let bytes_len = Bytes.length bytes_msg in
+      Lwt_io.printf "Sending message : %s\n" msg >>= fun _ ->
+      Lwt_unix.write socket bytes_msg 0 bytes_len >>= fun _ ->
+      send_handler socket
+  | None -> Lwt.return_unit
+
+let rec client_recv_handler socket =
+  let buffer = Bytes.create 1024 in
+  let* len = Lwt_unix.read socket buffer 0 1024 in
+  let empty_byte = '\000' in
+  let buffer =
+    Bytes.(
+      filter_unfilled_bytes buffer empty_byte
+      |> to_string |> String.lowercase_ascii)
+  in
+  match len with
+  | 0 ->
+      Lwt_unix.close socket >>= fun _ ->
+      Lwt_io.printl "Server disconnected\n" >>= fun _ -> Lwt.return_unit
+  | _ ->
+      Lwt.catch
+        (fun () ->
+          if buffer = ack then
+            Lwt_io.printf "Received message : %s\n" ack >>= fun _ ->
+            Lwt.choose [ client_recv_handler socket; send_handler socket ]
+          else
+            Lwt_unix.write socket ack_bytes 0 ack_len >>= fun _ ->
+            Lwt_io.printf "Received message : %s\n" buffer >>= fun _ ->
+            Lwt.choose [ client_recv_handler socket; send_handler socket ])
+        (fun _exn ->
+          Lwt_unix.close socket >>= fun _ ->
+          Lwt_io.printl "Server disconnected" >>= fun _ -> Lwt.return_unit)
+
+let rec server_recv_handler socket =
+  let buffer = Bytes.create 1024 in
+  let* len = Lwt_unix.read socket buffer 0 1024 in
+  let empty_byte = '\000' in
+  let buffer =
+    Bytes.(
+      filter_unfilled_bytes buffer empty_byte
+      |> to_string |> String.lowercase_ascii)
+  in
+  match len with
+  | 0 ->
+      Lwt_unix.close socket >>= fun _ ->
+      Lwt_io.printl "Client disconnected\n" >>= fun _ -> Lwt.return_unit
+  | _ ->
+      Lwt.catch
+        (fun () ->
+          if buffer = ack then
+            Lwt_io.printf "Received message : %s\n" ack >>= fun _ ->
+            Lwt.choose [ server_recv_handler socket; send_handler socket ]
+          else
+            Lwt_unix.write socket ack_bytes 0 ack_len >>= fun _ ->
+            Lwt_io.printf "Received message : %s\n" buffer >>= fun _ ->
+            Lwt.choose [ server_recv_handler socket; send_handler socket ])
+        (fun _exn ->
+          Lwt_unix.close socket >>= fun _ ->
+          Lwt_io.printl "Client disconnected" >>= fun _ -> Lwt.return_unit)
